1b448ab966786bd07edf54ea4f6eca6896fddcc2
viesapi <contact@viesapi.eu>
2023-11-09 00:18:43 +0100

    Add JSON format

--- example-web/main.js
+++ example-web/main.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 NETCAT (www.netcat.pl)
+ * Copyright 2022-2023 NETCAT (www.netcat.pl)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  *
  * @author NETCAT <firma@netcat.pl>
- * @copyright 2022 NETCAT (www.netcat.pl)
+ * @copyright 2022-2023 NETCAT (www.netcat.pl)
  * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 
@@ -28,8 +28,7 @@ const VIESAPI = require('viesapi-client');
 // Create client object and establish connection to the test system
 const viesapi = new VIESAPI.VIESAPIClient();
 
-const nip = '7171642051';
-const nip_eu = 'PL' + nip;
+const nip_eu = 'PL7171642051';
 
 async function fetchData() {
     return {
